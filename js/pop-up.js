a95fea09724efd535b749a97580112bc4967db7b
Aman Kumar <amankumarmaurya93@gmail.com>
2025-05-12 23:40:17 +0530

    added Pop up message

---
+++ js/pop-up.js
@@ -0,0 +1,7 @@
+ window.onload = function () {
+    document.getElementById('popup-overlay').style.display = 'flex';
+  };
+
+  function closePopup() {
+    document.getElementById('popup-overlay').style.display = 'none';
+  }
