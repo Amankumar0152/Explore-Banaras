4457c370da7283179bec524617f6c976e479cd26
Aman Kumar <amankumarmaurya93@gmail.com>
2025-06-23 22:40:41 +0530

    added Dark features for Daty Night

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,5 +1,14 @@
-  function toggleOtherField() {
-    const reason = document.getElementById("reason").value;
-    const otherField = document.getElementById("otherReasonContainer");
-    otherField.style.display = reason === "Other" ? "block" : "none";
-  }
+// Auto Dark Mode
+const hour = new Date().getHours();
+const body = document.getElementById('pageBody');
+if (hour >= 19 || hour < 6) {
+  body.classList.add('dark-mode');
+}
+
+// Tour Options toggle
+const purposeSelect = document.getElementById("purpose");
+const tourOptions = document.getElementById("tourOptions");
+
+purposeSelect.addEventListener("change", () => {
+  tourOptions.style.display = (purposeSelect.value === "Tour") ? "block" : "none";
+});
