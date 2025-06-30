c0fe0bb354236c2e33150a8c3627a379d5ab92fc
Aman Kumar <amankumarmaurya93@gmail.com>
2025-06-30 13:20:26 +0530

    added more cards

--- scripts/include.js
+++ scripts/include.js
@@ -14,6 +14,20 @@
 // include.js
 
 // Load Navbar
+// fetch('/components/navbar.html')
+//   .then(response => response.text())
+//   .then(data => {
+//     document.getElementById('navbar-placeholder').innerHTML = data;
+//   });
+
+// // Load Footer
+// fetch('/components/footer.html')
+//   .then(response => response.text())
+//   .then(data => {
+//     document.getElementById('footer-placeholder').innerHTML = data;
+//   });
+
+// Load Navbar
 fetch('/components/navbar.html')
   .then(response => response.text())
   .then(data => {
@@ -27,3 +41,21 @@ fetch('/components/footer.html')
     document.getElementById('footer-placeholder').innerHTML = data;
   });
 
+// Load Cards
+fetch('/cards/food.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('food-card').innerHTML = data;
+  });
+
+fetch('/cards/temples.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('temples-card').innerHTML = data;
+  });
+
+fetch('/cards/tours.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('tours-card').innerHTML = data;
+  });
