dc18fa64cbf8caa6e51d7ae921e4e8b0f15bace7
Aman Kumar <amankumarmaurya93@gmail.com>
2025-11-06 20:36:57 +0530

    removved comment out codes

--- scripts/include.js
+++ scripts/include.js
@@ -1,32 +1,3 @@
-// scripts/include.js
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const footerElement = document.getElementById("footer-placeholder");
-
-//   fetch("footer.html")
-//     .then(response => response.text())
-//     .then(data => {
-//       footerElement.innerHTML = data;
-//     })
-//     .catch(error => console.error("Error loading footer:", error));
-// });
-
-// include.js
-
-// Load Navbar
-// fetch('/components/navbar.html')
-//   .then(response => response.text())
-//   .then(data => {
-//     document.getElementById('navbar-placeholder').innerHTML = data;
-//   });
-
-// // Load Footer
-// fetch('/components/footer.html')
-//   .then(response => response.text())
-//   .then(data => {
-//     document.getElementById('footer-placeholder').innerHTML = data;
-//   });
-
 // Load Navbar
 fetch('/components/navbar.html')
   .then(response => response.text())
